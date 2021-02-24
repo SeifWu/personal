a11f681b50bc3cf6779169ca8cada3ef0819fa2f
Seif Wu <seif.wuxf@gmail.com>
2021-02-24 16:14:51 +0800

    feat: add seen models

--- seen/models.py
+++ seen/models.py
@@ -1,3 +1,29 @@
+import uuid
+
 from django.db import models
 
+
 # Create your models here.
+class Category(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=50)
+    parent = models.ForeignKey('self', on_delete=models.SET_NULL, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'seen_category'
+
+
+class WatchedObject(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=50)
+    # TODO 尝试使用 FilePathField
+    cover = models.CharField(max_length=255)
+    categories = models.ManyToManyField(Category)
+    seen_at = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'watched_object'
