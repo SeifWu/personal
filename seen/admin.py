1fa6dce4954cf8b26ff08e4b76b792fee8cd5bb6
Seif Wu <seif.wuxf@gmail.com>
2021-02-24 16:41:03 +0800

    feat: register tag

--- seen/admin.py
+++ seen/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Category, WatchedObject
+from .models import Category, WatchedObject, Tag
 
 admin.site.register(Category)
 admin.site.register(WatchedObject)
+admin.site.register(Tag)
